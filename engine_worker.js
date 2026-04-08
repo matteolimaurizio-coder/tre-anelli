043c838492912e5c88f59e6c01e0eaa062efa4b1
Claudiu Alexandru Mandru <mandru.2172420@studenti.uniroma1.it>
2026-04-08 12:52:29 +0200

    Milgliorata barra analisi #6

--- engine_worker.js
+++ engine_worker.js
@@ -1,65 +1,43 @@
-'use strict';
+importScripts('engine_v2.js');
 
-let isReady = false;
-
-var Module = {
-    onRuntimeInitialized: function () {
-        isReady = true;
-        postMessage({ type: 'ready' });
-    },
-    print:    function(text) { console.log('[C++]', text); },
-    printErr: function(text) { console.warn('[C++]', text); }
+Module.onRuntimeInitialized = function() {
+    Module.ccall('init_engine', 'null', [], []);
+    postMessage({ type: 'ready' });
 };
 
-try {
-    importScripts('engine_v2.js');
-} catch(e) {
-    postMessage({ type:'error', message: 'Impossibile caricare engine.js: ' + e.message });
-}
-
-function wasmCall(name, retType, argTypes, args) {
-    return Module.ccall(name, retType, argTypes, args);
+// Funzione di supporto per chiamare il C++
+function wasmCall(funcName, returnType, argTypes, args) {
+    return Module.ccall(funcName, returnType, argTypes, args);
 }
 
 self.onmessage = function(e) {
-    const msg = e.data;
-
-    if (!isReady) {
-        if (msg.type === 'calcola') setTimeout(() => self.onmessage(e), 100);
-        return;
-    }
-
-    switch (msg.type) {
-        case 'set_time':
-            wasmCall('set_time_limit_wasm', null, ['number'], [msg.ms | 0]);
-            break;
-
-        case 'set_ordering':
-            wasmCall('set_ordering_wasm', null,
-                ['number','number','number','number'],
-                [msg.cap|0, msg.win|0, msg.rot|0, msg.place|0]
-            );
-            break;
-
-        case 'calcola':
-            const { board, reserve, special, player, max_depth, is_beta, rotN, _callbackId } = msg;
-            const t0 = performance.now();
-
-            // Usiamo il 'array' magico di Emscripten (sicuro al 100%)
-            let result = wasmCall('calcola_mossa_wasm', 'number',
+    let msg = e.data;
+    
+    try {
+        if (msg.type === 'set_time') {
+            wasmCall('set_time', 'null', ['number'], [msg.ms]);
+        } 
+        else if (msg.type === 'set_ordering') {
+            wasmCall('set_ordering', 'null', ['number','number','number','number'], [msg.cap, msg.win, msg.rot, msg.place]);
+        } 
+        else if (msg.type === 'set_flags') {
+            wasmCall('set_flags', 'null', ['number','number','number','number'], [msg.pvs?1:0, msg.nmp?1:0, msg.lmr?1:0, msg.fut?1:0]);
+        } 
+        else if (msg.type === 'calcola') {
+            let res = wasmCall('calcola', 'number', 
                 ['array','array','array','number','number','number','number'],
-                [board, reserve, special, player | 0, max_depth | 0, is_beta | 0, rotN | 0]);
-
-            let eval_score = wasmCall('get_last_eval', 'number', [], []);
-            let depth      = wasmCall('get_last_depth', 'number', [], []);
-            let nodes      = wasmCall('get_node_count_wasm', 'number', [], []);
-            let tt_hits    = wasmCall('get_tt_hits_wasm', 'number', [], []);
-
-            const time_ms = Math.round(performance.now() - t0);
-
+                [msg.board, msg.reserve, msg.special, msg.player, msg.max_depth, msg.is_beta, msg.rotN]
+            );
+            let eval_score = wasmCall('get_eval', 'number', [], []);
+            
             postMessage({
-                type: 'mossa', result, eval_score, depth, nodes, tt_hits, time_ms, _callbackId
+                type: 'mossa',
+                result: res,
+                eval_score: eval_score,
+                _callbackId: msg._callbackId
             });
-            break;
+        }
+    } catch (err) {
+        console.error("Errore di comunicazione C++:", err);
     }
 };
